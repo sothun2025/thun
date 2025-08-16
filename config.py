404b2cc3f45e9407af95cf938eba51c891781ffa
sothun2025 <darahost91@gmail.com>
2025-08-16 12:06:44 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    SECRET_KEY = '49fa904d01d7'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'sothunthoeun1@gmail.com'
+    MAIL_PASSWORD = 'ffqr uvbo bzvj ocos'  # អ្នកត្រូវយក App password មកដាក់!
+    MAIL_DEFAULT_SENDER = 'sothunthoeun1@gmail.com' # optional but recommended
+    TELEGRAM_BOT_TOKEN = '8021006374:AAGruM-a23yXeo6gDeN-NoZzV4z0RtPppkk'
+    TELEGRAM_CHAT_ID = '@PP_su33'    # ប្តូរជា chat id ផ្ទាល់របស់អ្នក
+    MAIL_CONTACT_TO = 'sothunthoeun1@gmail.com'  # where contact messages go
+
